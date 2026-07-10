6d1c1c305e191266b6151146138442d7934af39b
Diego Garcia Seijo <diegogarciaseijo@gmail.com>
2026-07-10 18:27:27 +0200

    Fix operator input validation and division by zero handling

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -10,11 +10,12 @@ int main(void)
     printf("First number: ");
     scanf("%d", &number1);
     // Input for the operator until its a valid one
-    while ((operator != '+') && (operator != '-') && (operator != '*') && (operator != '/'))
+    do
     {
         printf("Operator (accepted '+', '-', '*' and '/'): ");
         scanf(" %c", &operator);
     }
+    while ((operator != '+') && (operator != '-') && (operator != '*') && (operator != '/'));
     // Input for the second number
     printf("Second number: ");
     scanf("%d", &number2);
@@ -37,9 +38,13 @@ int main(void)
         printf("%i %c %i = %i", number1, operator, number2, result);
     }
     // Division
-    else
+    else if (operator == '/' && number2 != 0)
     {
         result = number1 / number2;
         printf("%i %c %i = %i", number1, operator, number2, result);
+    }
+    else
+    {
+        printf("You cant divide by 0\n");
     }
 }
